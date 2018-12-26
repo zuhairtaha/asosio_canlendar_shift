6ad2bf8aaf71ef8be76ef4e128d2ddf93a5a7ee6
Zuhair Taha <admin@tahasoft.com>
2018-12-26 22:05:02 +0100

    Updates

---
+++ precache-manifest.7b065538f0d018f95cc27374ee2603ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88a41c00f67bb403de5e",
+    "url": "/asosio_canlendar_shift/static/css/main.8fd4f755.chunk.css"
+  },
+  {
+    "revision": "88a41c00f67bb403de5e",
+    "url": "/asosio_canlendar_shift/static/js/main.88a41c00.chunk.js"
+  },
+  {
+    "revision": "8c440b74bde975202965",
+    "url": "/asosio_canlendar_shift/static/css/1.2107c52d.chunk.css"
+  },
+  {
+    "revision": "8c440b74bde975202965",
+    "url": "/asosio_canlendar_shift/static/js/1.8c440b74.chunk.js"
+  },
+  {
+    "revision": "a706f033968c2c3209ef",
+    "url": "/asosio_canlendar_shift/static/js/runtime~main.a706f033.js"
+  },
+  {
+    "revision": "e3525a169bb66de660c64b689f430bcf",
+    "url": "/asosio_canlendar_shift/index.html"
+  }
+];
